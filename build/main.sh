b7fc78ed8625e340f461f452d79c214b6c226618
symac <symac@symacnoiMac.local>
2022-09-26 01:03:42 +0900

    create build scripts

---
+++ build/main.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "実行するには1個の引数が必要です。" 1>&2
+  exit 1
+fi
+
+tag=$1
+git checkout refs/tags/${tag}
+python make_pipeline.py ${tag}
+
